9d6eed1cee026cddb0562490f6e923d8e9b3007c
DevanshDaxini <devanshdaxini@gmail.com>
2026-02-10 12:52:36 -0800

    got player positions

--- src/builder.py
+++ src/builder.py
@@ -3,6 +3,8 @@ import time
 import os
 from nba_api.stats.endpoints import playergamelogs
 from nba_api.stats.static import players
+from nba_api.stats.endpoints import commonteamroster
+from nba_api.stats.static import teams
 
 # --- CONFIGURATION ---
 SEASONS = ['2022-23', '2023-24', '2024-25']
@@ -53,5 +55,52 @@ def fetch_all_game_logs():
     else:
         print("FAILED: No data found.")
 
+def fetch_player_positions():
+    """
+    Fetches the position (G, F, C) for every active player.
+    We do this by looping through all 30 NBA Teams' rosters.
+    """
+    POSITION_FILE = f'{DATA_FOLDER}/player_positions.csv'
+    
+    if os.path.exists(POSITION_FILE):
+        print(f"Positions file found at {POSITION_FILE}. Skipping download.")
+        return
+
+    print("\n--- FETCHING PLAYER POSITIONS (30 Teams) ---")
+    
+    # 1. Get all team IDs (using static.teams)
+    nba_teams = teams.get_teams()
+    all_rosters = []
+
+    # 2. Loop through every team
+    for team in nba_teams:
+        t_id = team['id']
+        t_name = team['full_name']
+        print(f"Fetching roster for: {t_name}...")
+        
+        try:
+            roster = commonteamroster.CommonTeamRoster(team_id=t_id, 
+                                                       season='2025-26')
+
+            df = roster.get_data_frames()[0]
+            
+            df = df[['PLAYER', 'PLAYER_ID', 'POSITION']]
+            
+            all_rosters.append(df)
+            
+            time.sleep(0.6)
+            
+        except Exception as e:
+            print(f"Error fetching {t_name}: {e}")
+
+    # 3. Save to CSV
+    if all_rosters:
+        master_roster = pd.concat(all_rosters, ignore_index=True)
+        master_roster.to_csv(POSITION_FILE, index=False)
+        print(f"SUCCESS: Saved {len(master_roster)} player positions to {POSITION_FILE}")
+    else:
+        print("FAILED: No roster data found.")
+
 if __name__ == "__main__":
     fetch_all_game_logs()
+    fetch_player_positions()
